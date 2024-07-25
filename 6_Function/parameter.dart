3cab8dd046592514f6e9e27579a6be3f5e84b90b
zavaultraz <zavazaneta@gmail.com>
2024-07-25 10:14:21 +0700

    Parameter

--- 6_Function/parameter.dart
+++ 6_Function/parameter.dart
@@ -1,3 +1,8 @@
 void luasPersegi(int panjang,double lebar){
+double luas = panjang* lebar;
+print("Luas persegi panjang adalah $luas");
+}
 
+void main(List<String> args) {
+  luasPersegi(10, 5.7);
 }
