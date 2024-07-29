5038529eafeef23be06468b85209e1242f0f0f14
zavaultraz <zavazaneta@gmail.com>
2024-07-29 13:44:52 +0700

    up

--- 4_Looping/break_continue.dart
+++ 4_Looping/break_continue.dart
@@ -1,5 +1,17 @@
 void main(List<String> args) {
-  for(int i=0;i<=10;i++){
+  //break
+  for (int i = 0; i <= 10; i++) {
+    if (i == 5) {
+      break; //jika i = 5 maka perulangan akan berhenti
+    }
+    print('loop ke-$i dan berhenti jika i = 5');
+  }
 
+  //continue
+  for (int i = 0; i <= 10; i++) {
+    if (i == 5) {
+      continue; // jika i = 5 maka perulangan akan berhenti atau di skip
+    }
+    print('loop ke-$i dan dilewati jika i = 5');
   }
 }
